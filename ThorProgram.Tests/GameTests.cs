76851b22a8911539e62dec2494c55c8829176a4d
baka <bakayarusama@gmail.com>
2020-01-23 23:36:43 +0100

    Add MoveThorToGiant test

--- ThorProgram.Tests/GameTests.cs
+++ ThorProgram.Tests/GameTests.cs
@@ -31,21 +31,29 @@ namespace ThorProgram.Tests
             Check.That(thorPosition).IsNotNull();
         }
         [Test]
-        public void shouldMoveThor()
+        public void ShouldMoveThorToGiant()
         {
             // Arrange 
             const int thorPositionByX = 3;
             const int thorPositionByY = 3;
+            const int giantPositionByX = 4;
+            const int giantPositionByY = 4;
             _game.SetContentPosition(thorPositionByX, thorPositionByY, ContentPosition.Thor);
-
+            _game.SetContentPosition(giantPositionByX, giantPositionByY, ContentPosition.Giant);
+            var giantPosition = new Position
+            {
+                X = giantPositionByX,
+                Y = giantPositionByY, 
+                Content = ContentPosition.Giant
+            };
+ 
             // Act
-            _game.MoveThorTo(Direction.North);
+            _game.MoveThorTo(giantPosition);
             var thorPosition = _game.Map.Find(location => location.Content.Equals(ContentPosition.Thor));
 
             // Assert
             Check.That(thorPosition).IsNotNull();
-            Check.That(thorPosition.Y).IsEqualTo(thorPositionByY-1);
-
+            Check.That(thorPosition).IsEqualTo(giantPosition);
         }
     }
 }
